8ba8770b0ea5fb42e0f51aa2d45e3dd86a80fabd
Maxime Thirouin <maxime.thirouin@gmail.com>
2012-03-08 21:30:32 +0100

    Update stylesheet from github personal pages

--- config.rb
+++ config.rb
@@ -1,5 +1,10 @@
 require "ceaser-easing"
-require "compass-recipes"
+
+#require "compass-recipes"
+# local under development compass recipes files
+compass_recipes_dir = File.join(File.dirname(__FILE__), '..', 'compass-recipes')
+require File.join(compass_recipes_dir, 'lib', 'compass-recipes')
+additional_import_paths = [ File.join(compass_recipes_dir, 'stylesheets') ]
 
 http_path = "/"
 css_dir = "stylesheets"
